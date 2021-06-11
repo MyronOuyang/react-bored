9128bb75aea9ff3461018403c909a8aeac4b8620
Myron <myronouyang@gmail.com>
2021-06-11 18:48:28 +0200

    [ADD] tile

---
+++ app/components/Tile/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Tile
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function Tile() {
+  return (
+    <>
+      <div>This is my Tile</div>
+    </>
+  );
+}
+
+Tile.propTypes = {};
+
+export default memo(Tile);
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -7,11 +7,13 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Tile from 'components/Tile';
 import messages from './messages';
 
 export default function HomePage() {
   return (
     <h1>
+      <Tile />
       <FormattedMessage {...messages.header} />
     </h1>
   );
